5e8ce6cfd9260ea4da75a545fe1f2458284f48df
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-06-08 12:26:44 -0300

    button light mode + script

--- Exercicio14/script.js
+++ Exercicio14/script.js
@@ -1,3 +1,9 @@
+function toggle() {
+    /*variável que representa o conteúdo html inteiro*/ 
+    const html = document.documentElement;
+    html.classList.toggle('light');
+}
+
 function insert(param){
     var actual = document.getElementById("result").innerHTML;
 
